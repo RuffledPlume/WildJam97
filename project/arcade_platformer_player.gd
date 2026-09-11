d98eb4f6e8f8ae77270bbec720b4c038162239ec
thatsoheeron <soheeron@gmail.com>
2026-09-11 19:57:19 -0400

    edited player jump velocity and tilemap layout

--- project/arcade_platformer_player.gd
+++ project/arcade_platformer_player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 @onready var sprite_animation = $player_sprite
 
@@ -10,7 +10,7 @@ func play(param):
 	sprite_animation.play(param)
 
 func _ready() -> void:
-	play("idle")
+	pass
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,8 +20,8 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("platformer_player_jump") and is_on_floor():
-		play("jump")
 		velocity.y = JUMP_VELOCITY
+		play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,6 +29,10 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		play("walking")
+		if Input.is_action_pressed("platformer_player_left"):
+			sprite_animation.flip_h = true
+		else:
+			sprite_animation.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		play("idle")
