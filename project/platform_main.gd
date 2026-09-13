2a8b850d2b997ea633b59beda45eef5d1bb87f6a
thatsoheeron <soheeron@gmail.com>
2026-09-13 01:14:11 -0400

    pivoted back to puzzle platformer, redid platform layout

--- project/platform_main.gd
+++ project/platform_main.gd
@@ -2,20 +2,22 @@ extends Node2D
 
 var picked_up = false
 var door_area_active = false
-var camera_progress_ratio = 0
 
 @onready var pick_up = %pick_up
 @onready var locked_door = %locked_door
-@onready var vertical_camera = %camera_path
+@onready var press_to_start = %press2start
+
+func start_game():
+	if Input.is_anything_pressed() and press_to_start.visible == true:
+		press_to_start.visible = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	vertical_camera.progress_ratio = 0
-	camera_progress_ratio = .0005
+	press_to_start.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	start_game()
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("platformer_player_interact") and door_area_active == true and picked_up == true:
@@ -27,16 +29,8 @@ func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 	picked_up = true
 	pick_up.visible = false
 
-
 func door_on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	door_area_active = true
 
 func door_on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	door_area_active = false
-
-
-func camera_on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	camera_progress_ratio = .001
-
-func camera_on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	camera_progress_ratio = .0005
