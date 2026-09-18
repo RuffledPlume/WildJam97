974444c91a40770964592ed0f4c127dd79f6844b
thatsoheeron <soheeron@gmail.com>
2026-09-18 04:32:17 -0400

    added end screens, restart function, and tutorial text

--- project/arcade_platformer_player.gd
+++ project/arcade_platformer_player.gd
@@ -2,17 +2,18 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -650.0
 
 @onready var sprite_animation = $player_sprite
 
 var can_doublejump = false
+var can_move = false
 
 func play(param):
 	sprite_animation.play(param)
 
 func _ready() -> void:
-	pass
+	can_move = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,27 +22,28 @@ func _physics_process(delta: float) -> void:
 		play("falling")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("platformer_player_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		play("jump")
-		can_doublejump = true
-	elif Input.is_action_just_pressed("platformer_player_jump") and can_doublejump == true and !is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		play("jump")
-		can_doublejump = false
+	if can_move == true:
+		if Input.is_action_just_pressed("platformer_player_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			play("jump")
+			can_doublejump = true
+		elif Input.is_action_just_pressed("platformer_player_jump") and can_doublejump == true and !is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			play("jump")
+			can_doublejump = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("platformer_player_left", "platformer_player_right")
-	if direction:
-		velocity.x = direction * SPEED
-		play("walking")
-		if Input.is_action_pressed("platformer_player_left"):
-			sprite_animation.flip_h = true
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("platformer_player_left", "platformer_player_right")
+		if direction:
+			velocity.x = direction * SPEED
+			play("walking")
+			if Input.is_action_pressed("platformer_player_left"):
+				sprite_animation.flip_h = true
+			else:
+				sprite_animation.flip_h = false
 		else:
-			sprite_animation.flip_h = false
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			play("idle")
 
-	move_and_slide()
+		move_and_slide()
--- project/platform_main.gd
+++ project/platform_main.gd
@@ -1,17 +1,28 @@
 extends Node2D
 
+var sign1_activated = false
+var sign2_activated = false
+var sign3_activated = false
+var sign4_activated = false
+var sign5_activated = false
+var sign6_activated = false
 var picked_up1 = false
 var picked_up2 = false
 var picked_up3 = false
-
 var side_door_locked1_activated = false
 var side_door_locked2_activated = false
 var locked_door_activated = false
 var doorway_activated = false
-
 var locked_door_opened = false
-
 var is_dead = false
+var textbox_up = false
+var timer_activated = false
+var end_screen_activated = false
+var win_screen_activated = false
+var secret1_got = false
+var secret2_got = false
+var secret3_got = false
+var won_game = false
 
 @onready var pick_up1 = %pick_up1
 @onready var pick_up2 = %pick_up2
@@ -22,61 +33,178 @@ var is_dead = false
 @onready var side_door_locked2 = %side_door_locked2
 @onready var side_door1_coll = %side_door1_coll
 @onready var side_door2_coll = %side_door2_coll
-
+@onready var text = %text
 @onready var platform_player = %player
-
 @onready var two_way_platform = %two_way_platform
 @onready var foreground_layer = %foreground
+@onready var textbox_animation = %AnimationPlayer
+@onready var timer = %Timer
+@onready var time_stat_label_death = %death_time_stat
+@onready var secret_stat_label_death = %death_secret_stat
+@onready var time_stat_label_won = %won_time_stat
+@onready var secret_stat_label_won = %won_secret_stat
 
-func start_game():
-	pass
+var secrets_got = 0
+var seconds_passed = 0
+var minutes_passed = 0
+var hours_passed = 0
+
+func textbox(param: String):
+	platform_player.can_move = false
+	text.text = ""
+	text.append_text(param)
+	textbox_animation.play("textbox_fadein")
+	textbox_up = true
+
+func add_text_to_textbox(sign_number, text_param: String):
+	if Input.is_action_just_released("platformer_player_interact") and sign_number == true and textbox_up == false:
+		textbox(text_param)
+
+func add_text_to_secret(sign_number, text_param: String, secret_param: int):
+	if Input.is_action_just_released("platformer_player_interact") and sign_number == true and textbox_up == false:
+		textbox(text_param)
+		if secret_param == 2:
+			secret2_got = true
+		elif secret_param == 3:
+			secret3_got = true
+		else:
+			pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	start_game()
+	pass
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("platformer_player_interact") and picked_up1 == true and side_door_locked1_activated == true:
 		side_door_locked1.play("open")
 		side_door1_coll.disabled = true
+	elif Input.is_action_just_pressed("platformer_player_interact") and picked_up1 != true and side_door_locked1_activated == true:
+		add_text_to_textbox(side_door_locked1_activated, "This door is locked! I wonder if there's a key nearby.")
+	
+	add_text_to_textbox(sign1_activated, "Press W to jump, press it twice to double jump!")
+	add_text_to_textbox(sign2_activated, "Some walls look different, and disappear when you collide into them")
+	add_text_to_textbox(sign3_activated, "If you want to drop from a thin platform, press D!")
+	add_text_to_textbox(sign4_activated, "There's a hidden key that unlocks all doors no matter the color.")
+	add_text_to_secret(sign5_activated, "Don't fall!", 2)
+	add_text_to_secret(sign6_activated, "You found me! Congrats!", 3)
+	
+	if Input.is_anything_pressed() and textbox_up == true:
+		textbox_animation.play("textbox_fadeout")
+		await textbox_animation.animation_finished
+		platform_player.can_move = true
+		textbox_up = false
 	
 	if Input.is_action_just_pressed("platformer_player_interact") and picked_up3 == true and side_door_locked2_activated == true:
 		side_door_locked2.play("open")
 		side_door2_coll.disabled = true
-	
+	elif Input.is_action_just_pressed("platformer_player_interact") and picked_up3 != true and side_door_locked2_activated == true:
+		add_text_to_textbox(side_door_locked2_activated, "Even the front door is locked?! I guess that makes sense...")
+
 	if Input.is_action_just_pressed("platformer_player_interact") and picked_up2 == true and locked_door_activated == true and locked_door_opened == false:
 		locked_door.play("open")
 		await locked_door.animation_finished
 		locked_door_opened = true
+	elif Input.is_action_just_pressed("platformer_player_interact") and picked_up2 != true and locked_door_activated == true and locked_door_opened == false:
+		add_text_to_textbox(locked_door_activated, "Another locked door...")
 	
 	if Input.is_action_just_pressed("platformer_player_interact") and locked_door_activated == true and locked_door_opened == true:
+		platform_player.can_move = false
 		await create_tween().tween_property(platform_player, "modulate:a", 0, .5).finished
 		platform_player.position = Vector2(2303.0, -688.0)
 		await create_tween().tween_property(platform_player, "modulate:a", 1, .5).finished
+		platform_player.can_move = true
 
 	if Input.is_action_just_pressed("platformer_player_interact") and doorway_activated == true and locked_door_opened == true:
+		platform_player.can_move = false
 		await create_tween().tween_property(platform_player, "modulate:a", 0, .5).finished
 		platform_player.position = Vector2(2303.0, -301.0) 
 		await create_tween().tween_property(platform_player, "modulate:a", 1, .5).finished
-	elif Input.is_action_just_pressed("platformer_player_interact") and doorway_activated == true and locked_door_activated == false:
+		platform_player.can_move = true
+	elif Input.is_action_just_pressed("platformer_player_interact") and doorway_activated == true and locked_door_opened == false:
+		platform_player.can_move = false
 		locked_door.play("open")
 		await locked_door.animation_finished
 		locked_door_opened = true
 		await create_tween().tween_property(platform_player, "modulate:a", 0, .5).finished
 		platform_player.position = Vector2(2303.0, -301.0)
 		await create_tween().tween_property(platform_player, "modulate:a", 1, .5).finished
+		platform_player.can_move = true
 
 	if Input.is_action_just_pressed("platformer_player_drop"):
 		two_way_platform.collision_enabled = false
 	elif Input.is_action_just_released("platformer_player_drop"):
 		two_way_platform.collision_enabled = true
 	
-	if is_dead == true:
+	if is_dead == true and end_screen_activated == false:
+		var hours = ""
+		var minutes = ""
+		var seconds = ""
+		platform_player.can_move = false
+		
+		if hours_passed < 10:
+			hours = "0" + str(hours_passed)
+		else:
+			hours = str(hours_passed)
+		
+		if minutes_passed < 10:
+			minutes = "0" + str(minutes_passed)
+		else:
+			minutes = str(minutes_passed)
+		
+		if seconds_passed < 10:
+			seconds = "0" + str(seconds_passed)
+		else:
+			seconds = str(seconds_passed)
+		
+		time_stat_label_death.text = hours + ":" + minutes + ":" + seconds
+		secret_stat_label_death.text = str(secrets_got) + "/3"
+		textbox_animation.play("death")
+		end_screen_activated = true
+		await textbox_animation.animation_finished
+	
+	if won_game == true and win_screen_activated == false:
+		var hours = ""
+		var minutes = ""
+		var seconds = ""
+		platform_player.can_move = false
+		
+		if hours_passed < 10:
+			hours = "0" + str(hours_passed)
+		else:
+			hours = str(hours_passed)
+		
+		if minutes_passed < 10:
+			minutes = "0" + str(minutes_passed)
+		else:
+			minutes = str(minutes_passed)
+		
+		if seconds_passed < 10:
+			seconds = "0" + str(seconds_passed)
+		else:
+			seconds = str(seconds_passed)
+		
+		time_stat_label_won.text = hours + ":" + minutes + ":" + seconds
+		secret_stat_label_won.text = str(secrets_got) + "/3"
+		textbox_animation.play("won")
+		win_screen_activated = true
+		await textbox_animation.animation_finished
+	
+	if Input.is_anything_pressed() and end_screen_activated == true:
 		get_tree().reload_current_scene()
+	if Input.is_anything_pressed() and win_screen_activated == true:
+		get_tree().reload_current_scene()
+	
+	if seconds_passed >= 60:
+		seconds_passed = 0
+		minutes_passed += 1
+	
+	if minutes_passed >= 60:
+		minutes_passed = 0
+		hours_passed += 1
 
 func foreground_on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("platform_player"):
@@ -138,7 +266,85 @@ func pickup4_on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_sha
 	picked_up2 = true
 	picked_up3 = true
 	pick_upSkeleton.visible = false
+	if secret3_got == false:
+		secrets_got += 1
+		secret3_got = true
 
 func _on_deathtrap_body_entered(body: Node2D) -> void:
 	if body.is_in_group("platform_player"):
 		is_dead = true
+
+
+func _on_sign_1_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign1_activated = true
+
+
+func _on_sign_1_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign1_activated = false
+
+
+func _on_sign_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign2_activated = true
+
+func _on_sign_2_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign2_activated = false
+
+
+func _on_sign_3_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign3_activated = true
+
+
+func _on_sign_3_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign3_activated = false
+
+
+func _on_sign_4_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign4_activated = true
+
+
+func _on_sign_4_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign4_activated = false
+
+
+func _on_sign_5_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign5_activated = true
+
+
+func _on_sign_5_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign5_activated = false
+		if secret2_got == true:
+			secrets_got += 1
+		else:
+			pass
+
+
+func _on_sign_6_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign6_activated = true
+
+
+func _on_sign_6_body_exited(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		sign6_activated = false
+		if secret3_got == true:
+			secrets_got += 1
+		else:
+			pass
+
+func _on_timer_timeout() -> void:
+	seconds_passed += 1
+
+
+func _on_win_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("platform_player"):
+		won_game = true
